a55384c653adb27df91446e2673b96075c66a675
3TuShKaT <93029714+3TuShKaT@users.noreply.github.com>
2021-11-03 19:14:46 +0200

    Add files via upload

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Console.WriteLine("Insert number");
+            string N = Console.ReadLine();
+            int[] array = N.Select(x => x - 48).ToArray();
+
+            List<int> formalList = new List<int>();
+            foreach (int i in array)
+            {
+                formalList.Add(i);
+                if (formalList.Equals(formalList)); //Перевірка на спільні елементи?????
+                {
+                    counter++;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Number of repetetive digits is: ", counter);
+        }
+    }
+}
