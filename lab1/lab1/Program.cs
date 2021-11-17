4513372c443d826361d0f6eb80782bc038674e8f
3TuShKaT <93029714+3TuShKaT@users.noreply.github.com>
2021-11-17 13:47:06 +0200

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Перевірити істинність вислову: "Цифри даного тризначного числа утворюють арифметичну прогресію". 
+
+Console.WriteLine("Введіть тризначне число: ");
+int number = int.Parse(Console.ReadLine());
+int firstItem, secondItem, thirdItem;
+
+firstItem = number / 100;
+secondItem = (number / 10 ) % 10;
+thirdItem = number % 10;
+
+if (firstItem + secondItem == thirdItem)
+{
+    Console.WriteLine("Арифметична прогресія вірна");
+}
+else
+{
+    Console.WriteLine("Число не утворює арифметичну прогресію");
+}
